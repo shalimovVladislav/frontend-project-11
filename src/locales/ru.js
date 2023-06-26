07fb5c42b8b951ecd3dfbc71c5bf4ca5136e0e3a
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-06-26 17:50:05 +0400

    add locales directory

---
+++ src/locales/ru.js
@@ -0,0 +1,13 @@
+export default {
+  translation: {
+    feedbackMessages: {
+      successed: 'RSS успешно загружен',
+      invalidURL: 'Ссылка должна быть валидным URL',
+      existingRSS: 'RSS уже существует',
+      notContainRSS: 'Ресурс не содержит валидный RSS',
+      networkError: 'Ошибка сети',
+    },
+    posts: 'Посты',
+    feeds: 'Фиды',
+  },
+};
