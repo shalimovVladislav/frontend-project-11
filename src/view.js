45f568b104824b538e9a17c7f37d63affb8ff812
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-07-01 16:09:57 +0400

    update src/view.js

--- src/view.js
+++ src/view.js
@@ -1,3 +1,5 @@
+import { reverse } from 'lodash';
+
 const formRender = (state, { input, button, feedbackMessage }) => {
   switch (state) {
     case 'filling':
@@ -8,11 +10,12 @@ const formRender = (state, { input, button, feedbackMessage }) => {
       input.classList.remove('is-invalid');
       input.setAttribute('readonly', 'true');
       button.disabled = true;
-      feedbackMessage.textContent = '';
       feedbackMessage.classList.remove('text-danger');
       break;
     case 'processed':
       feedbackMessage.classList.add('text-success');
+      input.value = '';
+      input.focus();
       break;
     case 'failed':
       input.removeAttribute('readonly');
@@ -25,13 +28,94 @@ const formRender = (state, { input, button, feedbackMessage }) => {
   }
 };
 const showMessage = (value, messageElement, i18nInstance) => {
+  if (!value) {
+    messageElement.textContent = '';
+    return;
+  }
   messageElement.textContent = i18nInstance.t(`feedbackMessages.${value}`);
 };
-const feedsRender = () => {
+const feedsRender = (feeds, container, i18nInstance) => {
+  if (container.innerHTML === '') {
+    const card = document.createElement('div');
+    card.classList.add('card', 'border-0');
 
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+
+    const cardTitle = document.createElement('h2');
+    cardTitle.classList.add('card-title', 'h4');
+    cardTitle.textContent = i18nInstance.t('feeds');
+
+    const listGroup = document.createElement('ul');
+    listGroup.classList.add('list-group', 'border-0', 'rounded-0');
+
+    cardBody.append(cardTitle, listGroup);
+    card.append(cardBody);
+    container.append(cardBody);
+  }
+
+  const newFeed = feeds[feeds.length - 1];
+
+  const listGroup = container.querySelector('.list-group');
+  const listItem = document.createElement('li');
+  listItem.classList.add('list-group-item', 'border-0', 'border-end-0');
+
+  const itemTitle = document.createElement('h3');
+  itemTitle.classList.add('h6', 'm-0');
+  itemTitle.textContent = newFeed.title;
+
+  const itemDescription = document.createElement('p');
+  itemDescription.classList.add('m-0', 'small', 'text-black-50');
+  itemDescription.textContent = newFeed.description;
+
+  listItem.append(itemTitle, itemDescription);
+  listGroup.prepend(listItem);
 };
-const postsRender = () => {
+const postsRender = (posts, currentPosts, container, i18nInstance) => {
+  if (container.innerHTML === '') {
+    const card = document.createElement('div');
+    card.classList.add('card', 'border-0');
 
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+
+    const cardTitle = document.createElement('h2');
+    cardTitle.classList.add('card-title', 'h4');
+    cardTitle.textContent = i18nInstance.t('posts');
+
+    const listGroup = document.createElement('ul');
+    listGroup.classList.add('list-group', 'border-0', 'rounded-0');
+
+    cardBody.append(cardTitle, listGroup);
+    card.append(cardBody);
+    container.append(cardBody);
+  }
+
+  const listGroup = container.querySelector('.list-group');
+  const newPosts = reverse(posts.slice(currentPosts.length));
+  newPosts.forEach((post) => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+
+    const itemLink = document.createElement('a');
+    itemLink.setAttribute('href', post.link);
+    itemLink.classList.add('fw-bold');
+    itemLink.dataset.id = post.id;
+    itemLink.setAttribute('target', '_blank');
+    itemLink.setAttribute('rel', 'noopener noreferrer');
+    itemLink.textContent = post.title;
+
+    const itemBtn = document.createElement('button');
+    itemBtn.setAttribute('type', 'button');
+    itemBtn.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+    itemBtn.dataset.id = post.id;
+    itemBtn.dataset.bsToggle = 'modal';
+    itemBtn.dataset.bsTarget = '#modal';
+    itemBtn.textContent = i18nInstance.t('action.view');
+
+    listItem.append(itemLink, itemBtn);
+    listGroup.prepend(listItem);
+  });
 };
 export {
   formRender,
