46c9bacb8b2d6ffd6dd44459b3c37ada037b1cba
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-07-05 19:01:43 +0400

    fix app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,10 @@
 import * as yup from 'yup';
-import onChange from 'on-change';
 import i18next from 'i18next';
 import axios from 'axios';
 import uniqueId from 'lodash/uniqueId';
 import resources from './locales/index';
 import parseRSS from './parser';
-import {
-  formRender,
-  showMessage,
-  feedsRender,
-  postsRender,
-} from './view';
+import getwatchedState from './view';
 
 export default () => {
   const defaultLanguage = 'ru';
@@ -19,124 +13,116 @@ export default () => {
     lng: defaultLanguage,
     debug: false,
     resources,
-  });
+  })
+    .then(() => {
+      yup.setLocale({
+        mixed: {
+          notOneOf: 'existingRSS',
+        },
+        string: {
+          url: 'invalidURL',
+        },
+      });
 
-  yup.setLocale({
-    mixed: {
-      notOneOf: 'existingRSS',
-    },
-    string: {
-      url: 'invalidURL',
-    },
-  });
+      const state = {
+        ui: {
+          form: {
+            state: 'filling',
+            message: null,
+          },
+          content: {
+            feeds: [],
+            posts: [],
+          },
+        },
+      };
 
-  const state = {
-    ui: {
-      form: {
-        state: 'filling',
-        message: null,
-      },
-      content: {
-        feeds: [],
-        posts: [],
-      },
-    },
-  };
+      const elements = {
+        form: document.querySelector('form'),
+        input: document.querySelector('#url-input'),
+        feedbackMessage: document.querySelector('.feedback'),
+        button: document.querySelector('[type="submit"]'),
+        feedsContainer: document.querySelector('.feeds'),
+        postsContainer: document.querySelector('.posts'),
+      };
 
-  const elements = {
-    input: document.querySelector('#url-input'),
-    feedbackMessage: document.querySelector('.feedback'),
-    button: document.querySelector('[type="submit"]'),
-    feedsContainer: document.querySelector('.feeds'),
-    postsContainer: document.querySelector('.posts'),
-  };
+      const watchedState = getwatchedState(state, elements, i18nInstance);
 
-  const watchedState = onChange(state, (path, value, prevValue) => {
-    switch (path) {
-      case 'ui.form.state':
-        formRender(value, elements);
-        break;
-      case 'ui.form.message':
-        showMessage(value, elements.feedbackMessage, i18nInstance);
-        break;
-      case 'ui.content.feeds':
-        feedsRender(value, elements.feedsContainer, i18nInstance);
-        break;
-      case 'ui.content.posts':
-        postsRender(value, prevValue, elements.postsContainer, i18nInstance);
-        break;
-      default:
-        throw new Error(`unknown state path ${path}`);
-    }
-  });
+      const validate = (url) => {
+        const feedLinks = watchedState.ui.content.feeds.map((feed) => feed.link);
+        const shema = yup.string().url().notOneOf(feedLinks);
+        return shema.validate(url);
+      };
+      const addProxy = (url) => {
+        const urlWithProxy = new URL('/get', 'https://allorigins.hexlet.app');
+        urlWithProxy.searchParams.set('url', url);
+        urlWithProxy.searchParams.set('disableCache', 'true');
+        return urlWithProxy.toString();
+      };
 
-  const form = document.querySelector('form');
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
+      elements.form.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-    const feedLinks = watchedState.ui.content.feeds.map((feed) => feed.link);
-    const shema = yup.string().url().notOneOf(feedLinks);
-    const formData = new FormData(e.target);
-    const url = formData.get('url').trim();
-    shema.validate(url)
-      .then((validURL) => {
-        watchedState.ui.form.message = null;
-        watchedState.ui.form.state = 'processing';
-        axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(validURL)}`)
-          .then(parseRSS)
-          .then(({ feed, posts }) => {
-            feed.link = validURL;
-            feed.id = uniqueId;
-            watchedState.ui.content.feeds.push(feed);
-            posts.forEach((post) => {
-              post.feedID = feed.id;
-              post.id = uniqueId();
-            });
-            watchedState.ui.content.posts.push(...posts);
-            watchedState.ui.form.message = 'successed';
-            watchedState.ui.form.state = 'processed';
-          })
-          .then(() => {
-            watchedState.ui.form.state = 'filling';
+        const formData = new FormData(e.target);
+        const url = formData.get('url').trim();
+
+        validate(url)
+          .then((validURL) => {
+            watchedState.ui.form.message = null;
+            watchedState.ui.form.state = 'processing';
+            axios.get(addProxy(validURL))
+              .then((response) => {
+                const { feed, posts } = parseRSS(response);
+                feed.link = validURL;
+                feed.id = uniqueId;
+                watchedState.ui.content.feeds.push(feed);
+                posts.forEach((post) => {
+                  post.feedID = feed.id;
+                  post.id = uniqueId();
+                });
+                watchedState.ui.content.posts.push(...posts);
+                watchedState.ui.form.message = 'successed';
+                watchedState.ui.form.state = 'processed';
+                watchedState.ui.form.state = 'filling';
+              })
+              .catch((error) => {
+                if (error.message === 'Network Error') {
+                  watchedState.ui.form.message = 'networkError';
+                }
+                if (error.isParseError) {
+                  watchedState.ui.form.message = 'notContainRSS';
+                }
+                watchedState.ui.form.state = 'failed';
+              });
           })
           .catch((error) => {
-            if (error.message === 'Network Error') {
-              watchedState.ui.form.message = 'networkError';
-            } else {
-              watchedState.ui.form.message = error.message;
-            }
+            watchedState.ui.form.message = error.errors;
             watchedState.ui.form.state = 'failed';
           });
-      })
-      .catch((error) => {
-        watchedState.ui.form.message = error.errors;
-        watchedState.ui.form.state = 'failed';
       });
-  });
-  const updateTracking = (feeds, currentPosts) => {
-    console.log('Tracking');
-    if (feeds.length !== 0) {
-      const currentPostsLinks = currentPosts.map((post) => post.link);
-      const promises = feeds.map((feed) => axios
-        .get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(feed.link)}`)
-        .then(parseRSS)
-        .then(({ posts }) => posts.filter((post) => !currentPostsLinks.includes(post.link)))
-        .then((newPosts) => newPosts.map((newPost) => {
-          newPost.feedID = feed.id;
-          newPost.id = uniqueId();
-          return newPost;
-        })));
-      const promise = Promise.all(promises);
-      promise
-        .then((contents) => watchedState.ui.content.posts.push(...contents.flat()))
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-    setTimeout(() => updateTracking(
-      watchedState.ui.content.feeds,
-      watchedState.ui.content.posts,
-    ), 5000);
-  };
-  updateTracking(watchedState.ui.content.feeds, watchedState.ui.content.posts);
+      const updateTracking = (feeds, currentPosts) => {
+        const currentPostsLinks = currentPosts.map((post) => post.link);
+        const promises = feeds.map((feed) => axios
+          .get(addProxy(feed.link))
+          .then((response) => {
+            const { posts } = parseRSS(response);
+            posts
+              .filter((post) => !currentPostsLinks.includes(post.link))
+              .forEach((newPost) => {
+                newPost.feedID = feed.id;
+                newPost.id = uniqueId();
+                watchedState.ui.content.posts.push(newPost);
+              });
+          }));
+        const promise = Promise.all(promises);
+        promise
+          .finally(() => {
+            setTimeout(() => updateTracking(
+              watchedState.ui.content.feeds,
+              watchedState.ui.content.posts,
+            ), 5000);
+          });
+      };
+      updateTracking(watchedState.ui.content.feeds, watchedState.ui.content.posts);
+    });
 };
